1c4e6d231fdf4fa17dd64cfd4a41e2a3390c503a
maxbubblegum47 <lorenzostigliano@gmail.com>
2021-09-08 18:26:46 +0200

    Editing profile in order to se user post from his personal profile. Removing some junk from the database. Removing Bio from the blog/models.py. Adding requirements.txt file.

--- users/models.py
+++ users/models.py
@@ -6,7 +6,6 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    bio = models.TextField(default="Bio")
 
     def __str__(self):
         return str(self.user)
